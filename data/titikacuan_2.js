d33bc1c7793d6115aedf8ed35ac64093c33035e3
sejatiaw <51725149+sejatiaw@users.noreply.github.com>
2020-12-03 13:59:13 +0700

    Add files via upload

---
+++ data/titikacuan_2.js
@@ -0,0 +1,8 @@
+var json_titikacuan_2 = {
+"type": "FeatureCollection",
+"name": "titikacuan_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.415726402953965, -7.159676079949273 ] } }
+]
+}
